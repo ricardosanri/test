bebefe8a6026ef8c11855a4e040cf06c746aedc5
Ricardo <ricardosanri@gmail.com>
2022-06-09 01:18:57 +0100

    first version of memory class

--- json.py
+++ json.py
@@ -1,1 +1,12 @@
-import json
+class Memory:
+    def __init__(self, content, mood, date, createdDate):
+        self.content = content
+        self.mood = mood
+        self.date = date
+        self.createdDate = createdDate
+    
+    def memory2json(memoryName):
+        pass
+
+
+    
